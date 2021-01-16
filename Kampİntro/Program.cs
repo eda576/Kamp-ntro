037446507d8263629c22eb0a1e6fdabe0095aee4
eda <edanurozltn@gmail.com>
2021-01-16 23:45:09 +0300

    Add project files.

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // type safety - tip güvenliği
+           // Do not repeat yourself - kendini tekrarlama
+           //değer tutucu, alias
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDün = 7.35;
+            double dolarBugün = 7.45;
+
+
+            if (dolarDün>dolarBugün)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDün<dolarBugün)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+            
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+      
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        // büyük harfle başlması naming convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi : "+ urun);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler, Sepete eklendi : " + urunAdi);
+        }
+    }
+}
