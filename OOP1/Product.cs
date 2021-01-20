f0abf22a1e9cece4586811040e93ff5ec38ab270
eda <edanurozltn@gmail.com>
2021-01-20 23:51:34 +0300

    20.01.2021 23.51

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet- kutunun altında olan noktacıklı şekil
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+
+        //CRUID operasyonları classların görevlerinden biridir
+    }
+}
